bce3012b4dafd1768a18617f0f8fdd17cc6ff46f
dedal.qq <dedal.qq@gmail.com>
2021-10-14 19:43:53 +0300

    add comments

--- testmodule/struct.go
+++ testmodule/struct.go
@@ -1,5 +1,6 @@
 package testmodule
 
+// TestStruct an example of structure for test
 type TestStruct struct {
 	a bool
 	b int
@@ -7,6 +8,7 @@ type TestStruct struct {
 	d []int
 }
 
+// NewTestStruct returns new TestStruct for tests
 func NewTestStruct(a bool, b int, c string, d []int) TestStruct {
 	return TestStruct{
 		a: a, b: b, c: c, d: d,
